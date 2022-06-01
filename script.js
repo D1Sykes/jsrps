25e7c7ab6d058629c72f93c8475f5920cbe3cfab
Sykes <peder.bakk1@gmail.com>
2022-06-01 21:48:48 +0200

    made changes to the script

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 function computerPlay() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    return choices = choices[Math.floor(Math.random()*DataTransferItemList.length)];
+    const number = (Math.random() * 3);
+    if (number <= 1) {
+        return "Rock";
+    } else if (number >= 2) {
+        return "Scissors";
+    }
+    else return "Paper";
 }
 
-console.log(computerPlay)
+let playerScore = 0;
+let computerScore = 0;
 
+function playRound(playerSelect, computerSelect) {
+    if (playerSelect == "Rock" && computerSelect == "Scissors") {playerScore++;
+    return `You win! Rock beats Scissors! The score is: ${playerScore} to ${computerScore}`
+  } else if (playerSelect == "Paper" && computerSelect == "Rock") {playerScore++;
+    return `You win! Paper beats Rock! The score is: ${playerScore} to ${computerScore}`
+  } else if (playerSelect == "Scissors" && computerSelect == "Paper") {playerScore++;
+    return `You win! Scissors beats Paper! The score is: ${playerScore} to ${computerScore}`
+  } else if (playerSelect == "Rock" && computerSelect == "Paper") {computerScore++;
+    return `You lose! Paper beats Rock! The score is: ${playerScore} to ${computerScore}`
+  } else if (playerSelect == "Paper" && computerSelect == "Scissors") {computerScore++;
+    return `You lose! Scissors beats Paper! The score is: ${playerScore} to ${computerScore}`
+  } else if (playerSelect == "Scissors" && computerSelect == "Rock") {computerScore++;
+    return `You lose! Rock beats Scissors! The score is: ${playerScore} to ${computerScore}`
+  }
+  else return `That's a draw, try again! The score is: ${playerScore} to ${computerScore}`
+}
